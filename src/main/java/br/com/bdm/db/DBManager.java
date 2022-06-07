67fdb8ee5484446c73a26b250ba2165ed9583b5d
carol <carolina.df@outlook.com>
2022-06-06 21:47:03 -0300

    Configuração e conexão com o banco

---
+++ src/main/java/br/com/bdm/db/DBManager.java
@@ -0,0 +1,32 @@
+package br.com.bdm.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    private static DBManager instance;
+
+    public DBManager() {
+    }
+
+    public static DBManager getInstance() {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+
+        return instance;
+    }
+
+
+    public static Connection criarConexao() {
+        Connection conexao;
+        try {
+            conexao = DriverManager.getConnection("jdbc:postgresql://localhost:15432/bdm-db?sslmode=disable",
+                    "postgres", "admin");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conexao;
+    }
+}
